6278bcf49867f70386ee5e2718fc2724e9d08d76
lutuandat <drianliz0411@gmail.com>
2018-11-21 10:39:22 +0700

    Update new content

---
+++ QuyetTienManage/QuyetTienManage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuyetTienManage.Startup))]
+namespace QuyetTienManage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
